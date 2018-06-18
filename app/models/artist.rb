a2982a8f7e327db0cfb64a20d3720741822e9ec7
liquidsword <audenrobertson@gmail.com>
2018-06-17 22:25:38 -0400

    updated models...artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
-class Artist < ApplicationRecord
+class Artist < ActiveRecord::Base
+    has_many :artwork
+    has_secure_password
+    
+    def slug
+        username.downcase.gsub(" ","-")
+    end
+    
+    def self.find_by_slug(slug)
+        User.all.find{|user| user.slug == slug}
+        end
+    end
 end
