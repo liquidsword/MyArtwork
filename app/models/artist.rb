35a6190ccad5acd2434fbcd0ef35bea514bee10f
liquidsword <audenrobertson@gmail.com>
2018-06-19 21:13:36 -0400

    cleaning up code
    hashing out possible unnecessary code

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,12 @@ class Artist < ActiveRecord::Base
     has_many :art_work
     has_secure_password
     
-    def slug
-        artist.downcase.gsub(" ","-")
-    end
+    #def slug
+        #artist.downcase.gsub(" ","-")
+    #end
     
-    def self.find_by_slug(slug)
-        Artist.all.find{|artist| artist.slug == slug}
-    end
+    #def self.find_by_slug(slug)
+        #Artist.all.find{|artist| artist.slug == slug}
+    #end
  
 end
