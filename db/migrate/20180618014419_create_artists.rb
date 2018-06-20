c4e7a0b343fff390073289485238c6f2c8426432
liquidsword <audenrobertson@gmail.com>
2018-06-19 23:35:47 -0400

    syntax error
    had to change password to password digest in my create_artists.db

--- db/migrate/20180618014419_create_artists.rb
+++ db/migrate/20180618014419_create_artists.rb
@@ -3,7 +3,7 @@ class CreateArtists < ActiveRecord::Migration[5.1]
     create_table :artists do |t|
       t.string :artist_name
       t.string :email
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
