d018ac6714923ec27a91375bbdf3f7b905a9d919
liquidsword <audenrobertson@gmail.com>
2018-06-25 00:15:23 -0400

    updated syntax
    updated find_by_id to fin

--- app/controllers/art_work_controller.rb
+++ app/controllers/art_work_controller.rb
@@ -1,43 +1,38 @@
 class ArtworkController < ApplicationController
     get '/artwork' do
-        if session[:artist_id]
+        redirect_if_not_logged_in
             @artwork = artwork.all
             erb :'artwork/index'
-        else
-            redirect to '/login'
-        end
     end
 
     get '/artwork/new' do
-        if session[:artist_id]
-            erb :'artwork/create_artwork'
-        else
-            redirect to '/login'
-        end
+        redirect_if_not_logged_in
+        @error_message = params[:error]
+        erb :'artwork/new'
     end
 
     post '/artwork' do
+        redirect_if_not_logged_in
         if params[:art]== ""
             redirect to '/artwork/new'
         else
-            artist = Artist.find_by_id(session[:artist_id])
+            artist = Artist.find(session[:artist_id])
             @artwork = Artwork.create(:art => params[:art], :artist_id => artist.id)
             redirect to "/artwork/#{@artwork.id}"
         end
     end
 
     get '/artwork/:id' do
-        if session[:artist_id]
-            @artwork = Artwork.find_by_id(params[:id])
-            erb :'/artwork/show_artwork'
-        else
-            redirect to '/login'
-        end
+        redirect_if_not_logged_in       
+        @artwork = Artwork.find(params[:id])
+        erb :'/artwork/show_artwork'
     end
 
     get '/artwork/:id/edit' do
+        redirect_if_not_logged_in
+        @error_message = params[:error]
         if session[:artist_id]
-            @artwork = Artwork.find_by_id(params[:id])
+            @artwork = Artwork.find(params[:id])
             if @artwork.artist_id == session[:artist_id]
                 erb :'artwork/edit_artwork'
             else
@@ -45,14 +40,14 @@ class ArtworkController < ApplicationController
             end
         else
             redirect to '/login'
-    end
+        end
     end
         
     patch '/artwork/:id' do
         if params[:art] == ""
             redirect to "/artwork/#{params[:id]}/edit"
         else
-            @artwork = Artwork.find_by_id(params[:id])
+            @artwork = Artwork.find(params[:id])
             @artwork.art = params[:art]
             @artwork.save
             redirect to "/artwork/#{@artwork.id}"
@@ -60,9 +55,9 @@ class ArtworkController < ApplicationController
     end
     
     delete '/artwork/:id/delete' do
-        @artwork = Artwork.find_by_id(params[:id])
+        @artwork = Artwork.find(params[:id])
         if session[:artist_id]
-            @artwork = Artwork.find_by_id(params[:id])
+            @artwork = Artwork.find(params[:id])
             if @artwork.artist_id == session[:artist_id]
                 @artwork.delete
                 redirect to '/artwork'
